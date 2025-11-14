6433f11016ed806329f03a4209ad6bf812005ac5
tanishasisodiya62-web <tanishasisodiya62@gmail.com>
2025-11-14 13:25:10 +0530

    data types

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    
+    + 3) // code readability should be high
+
+console.log("tanisha")
+
+
+let name = "tanisha"
+let age = 18
+let isLoggedIn = false
+let state; 
+
+// number => 2 to power 53 
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undifined => 
+// symbol => unique 
+
+
+// object
+
+console.log(typeof undifined); // undifined
+console.log(tyoeof null); // object
+
