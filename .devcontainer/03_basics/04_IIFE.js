090a217eedada5520701bd1067da1833b25e3876
tanishasisodiya62-web <tanishasisodiya62@gmail.com>
2025-11-21 16:03:39 +0530

    IIFE

---
+++ .devcontainer/03_basics/04_IIFE.js
@@ -0,0 +1,11 @@
+// Immediately Invoked function Expression (IIFE)
+
+
+(function chai(){
+    //named IIFE 
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} ) ('tanisha')
