867d985aaefeeed07707a0aaf394d9814a582662
tanishasisodiya62-web <tanishasisodiya62@gmail.com>
2025-11-23 10:43:39 +0530

    three

---
+++ .devcontainer/05_iteration/three.js
@@ -0,0 +1,39 @@
+//for of 
+
+//["", "", ""]
+//[{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greeting = "hello world!"
+for (const greet of greeting) {
+    //console.log(`each char is ${greet}`)
+}
+
+//Maps 
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United state of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const[key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+const myObject = {
+    'game1': 'NFS',
+    'game2': 'spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+// }
